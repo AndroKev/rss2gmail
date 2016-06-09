5e4930baba55c1407b8fe940e2eb1198eb20c86d
AndroKev <neureither.kevin@gmail.com>
2016-06-09 11:33:27 +0200

    First usable release!

--- README.md
+++ README.md
@@ -3,14 +3,11 @@ rss2imap
 
 An adaptation of rss2email that uses GMAIL directly.
 
-Forked from [rcarmo](https://github.com/rcarmo/rss2imap)
+Based on [rcarmo](https://github.com/rcarmo/rss2imap)
 
 # Main Differents to rss2imap:
 - works only with gmail
 - support subfolder(labels)
-
-# TODO:
-- get full articel
 
 # Special Thanks:
 - [rcarmo](https://github.com/rcarmo/rss2imap)
--- rss2gmail.py
+++ rss2gmail.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 """rss2gmail: get RSS feeds emailed to you
 https://github.com/AndroKev/rss2gmail
 
@@ -15,102 +17,165 @@ ___contributors__ = ["Dean Jackson", "Brian Lalor", "Joey Hess",
                      "Lindsey Smith (maintainer)", "Erik Hetzner",
                      "Aaron Swartz (original author)", "rcarmo"]
 
-### Import Modules ###
+# Import Modules
 
-import os, sys, time
+import os
+import sys
+import time
+import re
+import base64
 import argparse
 from datetime import datetime, timedelta
+import urllib2
 
 import imaplib
 import email
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+from email.header import Header
 import feedparser
+from HTMLParser import HTMLParser
 
 feedparser.USER_AGENT = "rss2gmail/" + __version__ + " +https://github.com/AndroKev/rss2gmail"
 VALID_CHAR = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
 DEFAULT_IMAP_FOLDER = "INBOX"
-VERBOSE=False
 warn = sys.stderr
 
-### Mail Functions ###
+
+# Mail Functions
+
+def mail_login():
+    try:
+        mail = imaplib.IMAP4_SSL("imap.gmail.com")
+        mail.login(GMAIL_USER, GMAIL_PASS)
+        return mail
+    except Exception:
+        print >>warn, "Couldn't login, please check login-details!"
+        sys.exit(1)
+
 
 def send(author, subject, published, labels, content, mail=None):
+
     if not mail:
-        try:
-            mail = imaplib.IMAP4_SSL("imap.gmail.com")
-            mail.login(GMAIL_USER, GMAIL_PASS)
-        except Exception:
-            print >>warn, "Counld't login into account, please check login-deatails!"
-            sys.exit(1)
+        mail = mail_login()
 
-    msg = MIMEMultipart()
-    msg['Subject'] = subject
-    msg['From']    = author
-    msg['To']      = GMAIL_USER
-    msg.attach(MIMEText(content, 'html'))
+    msg = MIMEMultipart('alternative')
+    msg['Subject'] = Header(subject, 'utf-8')
+    msg['From'] = author
+    msg['To'] = GMAIL_USER
 
+    if IMAGES_AS_ATTACHMETNS:
+        parser = Parser(tag='img', attr='src')
+        parser.feed(content)
+        img_nr = 0
+
+        for src in parser.attrs:
+            img_nr += 1
+            try:
+                if VERBOSE:
+                    print "    Load image: %s" % src
+                req = urllib2.Request(src.encode('utf-8'), headers={'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 5.1; it; rv:1.8.1.11) Gecko/20071127 Firefox/2.0.0.11'})
+                img_data = urllib2.urlopen(req)
+                try:
+                    img = MIMEImage(img_data.read())
+                except:
+                    img = MIMEImage(img_data.read(), _subtype="png")
+                # WORKAROUND: because the gmail-android-app dosen't load contend-id-images, it replace the <img/> with an image_name and add the image as an attachment!
+                content = re.sub(r'<img.*src="%s".*?/>' % re.escape(src), 'Bild_%d' % img_nr, content)
+                img.add_header('Content-Disposition', 'attachment; filename="Bild_%d"' % img_nr)
+                msg.attach(img)
+            except:
+                print >>warn, "Could not load image: %s" % src.encode('utf-8')
+                pass
+
+    msg.attach(MIMEText(content, 'html', 'utf-8'))
     mail.create(MAIN_GMAIL_FOLDER)
     rv, data = mail.append(MAIN_GMAIL_FOLDER, "", published, msg.as_string())
-    num=data[0].split(' ')[2][:-1]
+    num = data[0].split(' ')[2][:-1]
     if rv == "OK":
         for label in labels:
             mail.select(MAIN_GMAIL_FOLDER)
             mail.uid('STORE', num, '+X-GM-LABELS', "%s/%s" % (MAIN_GMAIL_FOLDER, label))
     return mail
 
+
 def delete_read(mail=None):
-    if mail:
-        if DELETE_READ_AFTER_DAYS != 0:
-            mail.select(MAIN_GMAIL_FOLDER)
-            yesterday = (datetime.now() - timedelta(days=DELETE_READ_AFTER_DAYS)).strftime("%d-%b-%Y")
-            res, data = mail.search(None, '(SEEN BEFORE %s UNFLAGGED)' % yesterday)
-            if res == 'OK':
-                    items = data[0].split()
-                    for i in items:
-                        res, data = mail.fetch(i, "(UID)")
-                        if data[0]:
-                            u = data[0].split(' ')[2][:-1] # get uid
-                            res, data = mail.uid('COPY', u, '[Gmail]/Papierkorb')
-                            if res == 'OK':
-                                res, data = mail.uid('STORE', u, '+FLAGS', '\\Deleted')
-                                mail.expunge()
 
-### Utility Functions ###
+    if not mail:
+        mail = mail_login()
 
-class InputError(Exception): pass
+    if VERBOSE:
+        print "Searching for read mails!"
+    mail.select(MAIN_GMAIL_FOLDER)
+    res, data = mail.search(None, '(SEEN UNFLAGGED)')
+    if res == 'OK':
+            for num in data[0].split():
+                mail.store(num, '+X-GM-LABELS', '\\Trash')  # with this the mail is moved to trash and after 30day it is deleted!
+            mail.expunge()
+            print "Deleted %d seen mails!" % len(data[0].split())
 
-def isstr(f): return isinstance(f, type('')) or isinstance(f, type(u''))
-def ishtml(t): return type(t) is type(())
-def contains(a,b): return a.find(b) != -1
 
-### Parsing Utilities ###
+# Utility Functions
+
+class InputError(Exception):
+    pass
+
+
+def isstr(f):
+    return isinstance(f, type('')) or isinstance(f, type(u''))
+
+
+def contains(a, b):
+    return a.find(b) != -1
+
+
+# Parsing Utilities
+
+class Parser(HTMLParser):
+    def __init__(self, tag='a', attr='href'):
+        HTMLParser.__init__(self)
+        self.tag = tag
+        self.attr = attr
+        self.attrs = []
+
+    def handle_starttag(self, tag, attrs):
+        if tag == self.tag:
+            attrs = dict(attrs)
+            if self.attr in attrs:
+                self.attrs.append(attrs[self.attr])
+
 
 def getContent(entry):
 
     conts = entry.get('content', [])
-
-    if entry.get('summary_detail', []):
-        conts += [entry.summary_detail]
+    conts += [entry.get('summary_detail', [])]
+    conts += [entry.get('description', [])]
 
     if conts:
         for c in conts:
-            if contains(c.type, 'html'): return ('HTML', c.value)
+            if contains(c.type, 'html'):
+                return c.value
 
-    return None
 
 def getFromEmail(feed_data, entry, firstlabel):
-    value = entry.get('author_detail', firstlabel) #feed.auto_detail fallback?
-    url=feed_data['feed'].get('link', None).replace("http://","").replace("https://","").replace("www.","").split('/')[0] # not beautiful, but it worked
+    value = entry.get('author_detail', firstlabel)
+    tm = str(time.time())
+
     if type(value) is str:
-        name=value
-        mail="author@%s" % url
+        name = value
     else:
         name = value.get('name', firstlabel)
-        mail = value.get('email', "%s@%s" % (name.replace(" ", "."), url ))
-    return "%s <%s>" % (name, mail)
 
-### Program Functions ###
+    if DEFAULT_EMAIL_FROM:
+        mail = DEFAULT_EMAIL_FROM
+    else:
+        mail = "%s@rss2gmail.com" % tm
+
+    return "\"%s\" <%s>" % (name.encode('utf-8'), mail)
+
+
+# Program Functions
 
 def feed_db_save(array):
     os.remove(FEEDFILE_PATH)
@@ -122,61 +187,74 @@ def feed_db_save(array):
                 line += "; " + str(i)
             f.write("%s\n" % line)
 
+
 def run(nosend, num=None):
     feeds = _list(True)
     mailserver = None
     mail = None
+
     try:
-        """
-        # We store the default to address as the first item in the feeds list.
-        # Here we take it out and save it for later.
-        default_to = ""
-        if feeds and isstr(feeds[0]): default_to = feeds[0]; ifeeds = feeds[1:]
-        else: ifeeds = feeds
-        """
         if num:
             ifeeds = [feeds[num]]
         else:
             ifeeds = feeds
+
         feednum = 0
+        articlenum = 0
 
         for f in ifeeds:
+            # try:
+            feednum += 1
+            if f[0].startswith('# '):
+                print 'I: Skipped    [%d/%d] "%s"' % (feednum, len(ifeeds), f[0][2:])
+                continue
+
+            print 'I: Processing [%d/%d] "%s"' % (feednum, len(ifeeds), f[0])
+            r = {}
+            r = feedparser.parse(f[0], f[2], f[3])
+            if r.get('status', None) == 304:
+                if VERBOSE:
+                    print "  skipped: %s (nothing changed)" % f[0]
+                continue
+
+            valid_char = VALID_CHAR + ".-_"
+            t = "".join(x for x in f[-1] if x in valid_char)
+            path = os.path.join(ARCHIVE_PATH, t)
+
             try:
-                feednum += 1
-                # if VERBOSE: print >>warn, 'I: Processing [%d] "%s"' % (feednum, f[0])
-                r = {}
-                r = feedparser.parse(f[0], f[2], f[3])
-                if r.get('status', None) == 304:
-                    if VERBOSE: print "skipped: %s" % f[0]
-                    continue
-
-                t = "".join(x for x in f[0] if x in VALID_CHAR)
-                path = os.path.join(ARCHIVE_PATH, t)
-                seen = open(path, 'r').readlines()
-
-                r.entries.reverse()
                 for entry in r.entries:
-                    uid = entry.get('id', entry.get('link', entry.get('title', None)))
-                    if uid+'\n' in seen:continue
+                    seen = open(path, 'r').read().decode('utf-8')
+                    uid = entry.get('title', entry.get('link', entry.get('id', None)))
+
+                    if seen.find(uid) != -1:
+                        if VERBOSE:
+                            print "  skipped: %s (already seen)" % uid
+                        continue
+
                     # new entry found:
-                    title = entry.get('title', None).strip()
-                    puplished = entry.get('published_parsed', time.localtime())
-                    author = getFromEmail(r, entry, f[4])
-                    article_link = entry.get('link', r['feed'].get('link', ""))
 
-                    msg = unicode("""<h1><a href="%s" title="%s">%s</a></h1><br />
-                    """ % (article_link, title, title))
-                    msg += getContent(entry)[1]
-
+                    articlenum += 1
+                    if VERBOSE:
+                        print "  new article: %s" % uid
                     if not nosend:
-                        mail = send(author, title, puplished, f[4:], msg.encode('ascii', 'xmlcharrefreplace'))
+                        title = entry.get('title', None).strip()
+                        puplished = entry.get('published_parsed', time.localtime())
+                        author = getFromEmail(r, entry, f[4])
+                        article_link = entry.get('link', r['feed'].get('link', ""))
+                        body = '<h1 class="header"><a href="%s">%s</a></h1>\n' % (article_link, title)
+                        body += getContent(entry)
+                        mail = send(author, title, puplished, f[4:], body, mail)
 
                     with open(path, 'a') as _file:
-                        _file.write("%s\n" % uid)
+                        _file.write("%s\n" % uid.encode('utf-8'))
+                        _file.close()
+            except:
+                print >>warn, "There was an Error on entry %s on Feed: %s" % (entry, f[0])
+                continue
 
-                f[2], f[3] = r.get('etag', None), r.get('modified', None)
-            except (KeyboardInterrupt, SystemExit):
-                raise
+            f[2], f[3] = r.get('etag', None), r.get('modified', None)
+
+        print 'Found %d new articles!' % articlenum
         feed_db_save(ifeeds)
 
     finally:
@@ -187,33 +265,40 @@ def run(nosend, num=None):
             except:
                 mail.logout()
 
-def add(add_list):
-    valid_char = VALID_CHAR + ".-_/ "
 
+def add(add_list):
     feeds = _list(True)
+
     for feed in feeds:
         if feed[0] == add_list[0]:
-            print "This feed already exists!"
+            print "ERROR: '%s' already exists!" % add_list[0]
             exit(1)
 
-    d = feedparser.parse(add_list[0])
-    labels = "".join(x for x in d['feed'].get('title', None) if x in valid_char) # get the Mainlabel(websitetitle)
-    for l in add_list[1:]: #add the other labels
-        labels += "; " + "".join(x for x in l if x in valid_char)
-    feeds.append([add_list[0], FULLFEED, d.get('etag', None), d.get('modified', None), labels])
-    feed_db_save(feeds)
+    try:
+        d = feedparser.parse(add_list[0])
 
-    if ADD_ARCHIVE_NEW_FEED:
-        t = "".join(x for x in add_list[0] if x in VALID_CHAR)
-        path = os.path.join(ARCHIVE_PATH, t)
-        with open(path, 'a') as f:
-            for item in d.get('entries', None):
-                f.write("%s\n" % item.get('id', item.get('title', '')))
+        main_label = d['feed'].get('link', d['feed'].get('title', add_list[0])).strip().split('//')[1]
+        valid_char = VALID_CHAR + ".-_/ "
+        labels = "".join(x for x in main_label if x in valid_char)  # get the Mainlabel(websitetitle)
+        for l in add_list[1:]:  # add the other labels
+            labels += "; " + "".join(x for x in l if x in valid_char)
 
-def _list(array=False): #is also used to load the db!
+        feeds.append([add_list[0], FULLFEED, d.get('etag', None), d.get('modified', None), labels])
+        feed_db_save(feeds)
+
+        if ADD_ARCHIVE_NEW_FEED:
+            valid_char = VALID_CHAR + ".-_"
+            t = "".join(x for x in main_label if x in valid_char)
+            path = os.path.join(ARCHIVE_PATH, t)
+            with open(path, 'a') as f:
+                for item in d.get('entries', None):
+                    f.write("%s\n" % (item.get('title', item.get('link', item.get('id', ''))).encode('utf-8')))
+    except:
+        print "ERROR: adding url %s" % add_list[0]
+
+
+def _list(array=False):  # is also used to load the db!
     feeds = []
-    # print("Nr.)  url  labels  fullfeed")
-    # print("="*100)
     for index, l in enumerate(open(FEEDFILE_PATH, 'r').readlines(), 1):
         line = l.strip()
         if line != "":
@@ -221,16 +306,17 @@ def _list(array=False): #is also used to load the db!
             if array:
                 feeds.append(v)
             else:
-                labels=v[4]
+                labels = v[4]
                 for l in v[5:]:
                     labels += ", " + l
-                print "%s.) %s [%s] %s" %(str(index), v[0].strip(), labels, v[1])
+                print "%s.) %s [%s] %s" % (str(index), v[0].strip(), labels, v[1])
     if array:
         return feeds
 
+
 def reset(nr):
-    nr-=1
-    feeds=_list(True)
+    nr -= 1
+    feeds = _list(True)
     if nr < 0:
         print "ID has to be equale to or higher than 1"
     elif nr > len(feeds):
@@ -243,8 +329,9 @@ def reset(nr):
         path = os.path.join(ARCHIVE_PATH, t)
         if os.path.exists(path):
             os.remove(path)
-            print('The feed "%s" was sucessfull reseted!' % url)
+            print 'The feed "%s" was sucessfull reseted!' % url
         open(path, 'w').close()
+
 
 def toggleactive(nr, mode):
     nr -= 1
@@ -269,8 +356,9 @@ def toggleactive(nr, mode):
 
         feed_db_save(feeds)
 
+
 def delete(nr):
-    nr-=1
+    nr -= 1
     feeds = _list(True)
     if nr < 0:
         print "ID has to be equale to or higher than 1"
@@ -286,27 +374,32 @@ def delete(nr):
             os.remove(path)
 
         del feeds[nr]
-        # print feeds
         feed_db_save(feeds)
-        print('The feed "%s" was sucessfull deleted!' % url)
+        print'The feed "%s" was sucessfull deleted!' % url
+
 
 def email(addr):
     feeds, feedfileObject = load()
-    if feeds and isstr(feeds[0]): feeds[0] = addr
-    else: feeds = [addr] + feeds
+    if feeds and isstr(feeds[0]):
+        feeds[0] = addr
+    else:
+        feeds = [addr] + feeds
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter)
-    parser.add_argument("-c", "--configfile", help="Path to your config-file", metavar="<file>")
+    parser.add_argument("--configfile", help="Path to your config-file", metavar="<file>")
     parser.add_argument("--add", help="Add a Feedurl!", metavar="feedurl LABELS", nargs="+")
     parser.add_argument("--no-send", help="Only load the feed, without sending them", action="store_true")
-    parser.add_argument("-l", "--list", action="store_true")
-    parser.add_argument("-d", "--delete", help="Delete a Feedurl", metavar="<nr>", type=int)
+    parser.add_argument("--list", action="store_true")
+    parser.add_argument("--delete", help="Delete a Feedurl", metavar="<nr>", type=int)
+    parser.add_argument("--delete_read", help="Delete read mails from gmail", action="store_true")
     parser.add_argument("--reset", help="Reset a Feed-Archive-File", metavar="<nr>", type=int)
     parser.add_argument("--enable", help="Enable a Feed", metavar="<nr>", type=int)
     parser.add_argument("--disable", help="Disable a Feed", metavar="<nr>", type=int)
     parser.add_argument("--verbose", help="Get more information!", action="store_true")
-    parser.add_argument("-V", "--version", action='version', version='%(prog)s {version}'.format(version=__version__))
+    parser.add_argument("--version", action="version", version="%(prog)s {version}".format(version=__version__))
+    parser.add_argument("integers", nargs="?", metavar="[num]", type=int)
     args = parser.parse_args()
 
     # Read options from config file if present.
@@ -331,10 +424,11 @@ if __name__ == '__main__':
         print """It seams that you run the tool the first time.
 Please edit the config file to your need and start the tool again!"""
     else:
-        if args.verbose:
-            VERBOSE=True
+        VERBOSE = args.verbose
         if args.list:
             _list()
+        elif args.delete_read:
+            delete_read()
         elif args.delete >= 0:
             delete(args.delete)
         elif args.enable >= 0:
